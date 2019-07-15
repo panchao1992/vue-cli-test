75b711da14cbcaaac82d79770b6111c1fc5576dc
潘超 <wb-pc541388@alibaba-inc.com>
2019-07-15 11:31:22 +0800

    平移动画

--- src/components/transition.vue
+++ src/components/transition.vue
@@ -1,28 +1,39 @@
 <template>
     <div id="main">
-        <transition>
-            <h1 id="ele">1</h1>
-        </transition>
+        <div ref="dom" class="begin">
+            <h1>1111111111</h1>                
+        </div>
+        <button @click="move">move</button>
     </div>
 </template>
 <script>
 export default {
     data(){
         return {
-
+            showFlag:false,
         }
     },
     created(){
         
     },
-    computed:{
+    mounted(){
         
     },
     methods:{
-        
+        move(){
+            // this.$refs.dom.style['transform'] = `translate3d(0,100px,0)`
+            // this.$refs.dom.style['transition'] = `transform 1s`
+            this.$refs.dom.classList.add('end')
+        }
     }
 }
 </script>
 <style scoped>
-
+.begin{
+    transform:translate3d(0,0,0);
+}
+.end{
+    transform:translate3d(0,100px,0);
+    transition:transform 3s;
+}
 </style>
